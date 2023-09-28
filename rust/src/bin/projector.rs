54e3d02edf61cdbc6a1871c8b98f0d424342cfce
nixpig <143995476+nixpig@users.noreply.github.com>
2023-09-28 07:18:30 +0100

    chore: clean up use of parser

--- rust/src/bin/projector.rs
+++ rust/src/bin/projector.rs
@@ -1,9 +1,10 @@
 use clap::Parser;
+// the `rust` on the line below is the name of the package, which in this case is `rust`
+// because the project was created in a directory called `rust`
+use rust::opts::Opts;
 
 fn main() {
-    // the `rust` on the line below is the name of the package, which in this case is `rust`
-    // because the project was created in a directory called `rust`
-    let opts = rust::opts::Opts::parse();
+    let opts = Opts::parse();
 
     println!("options: {:?}", opts);
 }
